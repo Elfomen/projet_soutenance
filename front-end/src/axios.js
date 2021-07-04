63511cdf245eb86e92b7c6119783d48a3096b0a5
elfomen <elfomenprofessor@gmail.com>
2021-07-04 11:42:21 +0100

    Modified axios

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const event = axios.create({
-    //baseURL : "https://frozen-wave-37700.herokuapp.com"
-    baseURL : "http://localhost:5000"
+    baseURL : "https://frozen-wave-37700.herokuapp.com"
+    //baseURL : "http://localhost:5000"
 })
 
 export default event
